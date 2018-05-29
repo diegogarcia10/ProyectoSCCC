49092b566d5f259d83333d285bee00fff5631b92
Ar-Montalvo <ar.funes@yahoo.com>
2018-05-29 16:09:14 -0600

    diagrama de clases edicion

--- apps/cultivo/models.py
+++ apps/cultivo/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Categorizacion(models.Model)
+    categoria = models.IntegerField()
+
+class MineralesDelSuelo(models.Model):
+    humedad = Categorizacion
+    minerales = Categorizacion
+
+class TipoSuelo(models.Model)
+    tipoSuelo = models.CharField(max_lenght=50)
+    isHomogeneo = models.BooleanField()
+    textura = Categorizacion
+    degradacion = Categorizacion
+    aradoDelSuelo = Categorizacion
+
+class Topografia(models.Model)
+    topografia = models.CharField(max_length=50)
+    descipcion = models.CharField(max_length=200)
+    temperaturaProm = models.DecimalField()
+    altitud = models.DecimalField()
+    lluvias = Categorizacion
+    radiacion = Categorizacion
+
+
+
