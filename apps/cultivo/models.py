1e2166c9d27a4755a96e20ca73628e2bdab74057
Ar-Montalvo <ar.funes@yahoo.com>
2018-06-05 13:14:03 -0600

    Diagrama terminado, Solo que aun no comprendo el uso de ForeignKey y ManyToOneRef
    si se necesita cambios digamelo.

--- apps/cultivo/models.py
+++ apps/cultivo/models.py
@@ -1,55 +1,142 @@
+from _ast import mod
+
 from django.db import models
 
 # Create your models here.
-class Categorizacion(models.Model)
+
+
+class Categorizacion(models.Model):
     categoria = models.IntegerField()
 
-class MineralesDelSuelo(models.Model):
-    humedad = Categorizacion
-    minerales = Categorizacion
 
-class TipoSuelo(models.Model)
-    tipoSuelo = models.CharField(max_lenght=50)
-    isHomogeneo = models.BooleanField()
-    textura = Categorizacion
-    degradacion = Categorizacion
-    aradoDelSuelo = Categorizacion
 
-class Topografia(models.Model)
-    topografia = models.CharField(max_length=50)
+class User(models.Model):
+    nameUser = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=200)
+    psword = models.CharField(max_length=30)
+
+
+#Factor Suelo
+
+
+class Topografia(models.Model):
+    topografia = models.CharField(max_length=30)
     descipcion = models.CharField(max_length=200)
-    temperaturaProm = models.DecimalField()
-    altitud = models.DecimalField()
-    lluvias = Categorizacion
-    radiacion = Categorizacion
+    temperaturaProm = models.DecimalField(max_digits=8, decimal_places=2)
+    altitud = models.DecimalField(max_digits=8, decimal_places=2)
+    lluvias = models.ForeignKey(Categorizacion)
+    radiacion = models.ForeignKey(Categorizacion)
 
-class Fertilizante()
-    porcentajeN = models.DecimalField()
-    gramP = models.DecimalField()
-    gramK = models.DecimalField()
 
-class ManejoDelSuelo(models.Model)
-    muestreo = models.IntegerField()
-    recuperacion = Categorizacion
-    espaciamiento = Categorizacion
-    tipoSuelo = TipoSuelo
+class Suelo(models.Model):
+    tipoSuelo = models.CharField(max_lenght=30)
+    esHomogeneo = models.BooleanField()
+    textura = models.ForeignKey(Categorizacion)
+    degradacion = models.ForeignKey(Categorizacion)
+    ventilacion = models.ForeignKey(Categorizacion)
+    estaEn = models.OneToOneRel(Topografia)
 
-class CondicionesClimaticas(models.Model)
-    periodosSequia = Categorizacion
-    hayInundacion = models.BooleanField()
-    temporadaDeFrio = models.BooleanField()
 
-class PlagasYEnfermedades(models.Model)
-    nombrePlaga = models.CharField(max_lenght = 50)
-    recomendacion = models.CharField(max_lenght = 200)
+class Fertilizante(models.Model):
+    nitrogeno = models.DecimalField(max_digits=8, decimal_places=2)
+    fosfotoGramP = models.DecimalField(max_digits=8, decimal_places=2)
+    potasioGramK = models.DecimalField(max_digits=8, decimal_places=2)
 
-class FactorAnual(models.Model)
-    anomaliasEnPlantacion = models.CharField(max_lenght=50)
+
+class ManejoDelSuelo(models.Model):
+    muestreo = models.CharField(max_length=200)
+    recuperacion = models.BooleanField()
+    espaciamiento = models.ForeignKey(Categorizacion)
+    fertilizante = models.ManyToManyRel(Fertilizante)
+
+
+class FactorControlable(models.Model):
+    profundidadSiembra = models.DecimalField(max_digits=3, decimal_places=2)
+    anteriorUsoDelSuelo = models.IntegerField()
+
+
+class Clima(models.Model):
+    fenomenoClimatico = models.CharField(max_length=50)
+    indiceSequia = models.IntegerField()
+    indiceInundacion = models.IntegerField()
+    tempMax = models.IntegerField()
+    tempMin = models.IntegerField()
+
+
+class PlagaOEnfermedad(models.Model):
+    nombrePlagaOEnfermedad = models.CharField(max_lenght=50)
+    recomendacion = models.CharField(max_lenght=200)
+
+
+class CondicionAnual(models.Model):
+    anomaliasEnLaPlantacion = models.CharField(max_lenght=50)
     fechaQueEmerge = models.DateField()
     hayMalezas = models.BooleanField()
-    condicionesClimaticas = CondicionesClimaticas
-    plagasYEnfermedades = PlagasYEnfermedades
-    
+    fenomenoClima = models.ManyToOneRel(Clima)
+    plagasYEnfermedades = models.ManyToOneRel(PlagaOEnfermedad)
+
+
+class MaterialGenetico(models.Model):
+    nombreMaterial = models.CharField(max_length=50)
+    potencialAzucarero = models.IntegerField()
+
+
+class DensidadCañera(models.Model):
+    calidad = models.ForeignKey(Categorizacion)
+    siembra = models.ForeignKey(Categorizacion)
+    yema = models.IntegerField()
+
+
+class SimulacionCultivo(models.Model):
+    nombreSimulacionCultivo = models.CharField(max_length=30)
+    fechaSiembra = models.DateField()
+    fc = models.ManyToOneRel(FactorControlable)
+    ca = models.ManyToOneRel(CondicionAnual)
+    dc = models.ManyToOneRel(DensidadCañera)
+    mg = models.ManyToManyRel(MaterialGenetico)
+
+
+class Rendimiento(models.Model):
+    pesoPromTalloGram = models.DecimalField(max_digits=8, decimal_places=2)
+    numPlantSemb = models.IntegerField()
+    numPlantGermi = models.IntegerField()
+    rendCaniaIni = models.DecimalField(max_digits=8, decimal_places=2)
+    rendCaniaFin = models.DecimalField(max_digits=8, decimal_places=2)
+    rendAzuIni = models.DecimalField(max_digits=8, decimal_places=2)
+    rendAzuFin = models.DecimalField(max_digits=8, decimal_places=2)
+
+
+class CicloProduccion(models.Model):
+    ciclosDeProduccion = models.IntegerField()
+    fechaDeCorta = models.DateField()
+    rendimiento = models.OneToOneRel(Rendimiento)
+
+
+class Cosecha(models.Model):
+    nombreSimulacionCosecha = models.CharField(max_length=30)
+    edadDeCorta = models.IntegerField()
+    esCorteManual = models.BooleanField()
+    esQuemada = models.BooleanField()
+    simulacionCultivo = models.ManyToOneRel(SimulacionCultivo)
+    ciclosDeProduccion = models.ForeignKey(CicloProduccion)
+
+
+
+
+#
+# class Caña(models.Model):
+#     fotosintesis = models.DecimalField(max_digits=8, decimal_places=2)
+#     crecimiento = models.DecimalField(max_digits=8, decimal_places=2)
+#     floracion = models.ForeignKey(Categorizacion)
+#     respiracion = models.ForeignKey(Categorizacion)
+#     absorcionMineral = models.ForeignKey(Categorizacion)
+#     elongacion = models.ForeignKey(Categorizacion)
+#     factoresExternos = FactoresExternos
+#     materialGenetico = MaterialGenetico
+#     densidadCañera = DensidadCañera
+#
+#
+#     #simulacion Cultivo
 
 
 
